156d5781ffbca5aa6d3a37b679bf82a37321ea2a
unknown <justyna.kuznikk@gmail.com>
2021-02-06 16:40:02 +0100

    entity :: user entity implementation

--- src/main/java/projects/beauty/shop/domain/model/User.java
+++ src/main/java/projects/beauty/shop/domain/model/User.java
@@ -1,4 +1,32 @@
 package projects.beauty.shop.domain.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString(exclude = "password")
+@EqualsAndHashCode(of="username")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
 }
